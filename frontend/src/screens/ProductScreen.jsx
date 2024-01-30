a8d7f290f8f265e6ec1309524e95f0dd2bb725c7
Ashish kumar <ashishpurbey221806@gmail.com>
2024-01-30 21:11:45 +0530

    server setup

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -56,7 +56,6 @@ const ProductScreen = () => {
                     </ListGroup.Item>
                     <ListGroup.Item>
                         <Button className='btn-block' type='button' disabled={product.countInStock ==0}>Add To Cart
-
                         </Button>
                     </ListGroup.Item>
                 </ListGroup>
