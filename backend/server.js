adf1e91de65f1d5da30bd4e16f1142423e508dcb
Ashish kumar <ashishpurbey221806@gmail.com>
2024-02-01 14:26:25 +0530

    middleware , controller and router setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT;
 
 connectDB();
@@ -10,13 +11,12 @@ connectDB();
 const app = express();
 
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
+app.get('/',(req,res)=>{
+    res.send('Api is running..');
 })
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((product)=>product._id==req.params.id);
-    res.json(product);
-})
+app.use('/api/products',productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,()=> console.log(`server is running on port ${port}`));
